fd0045c2e1fec53a883f06cdf61953e3ce55dc00
x5676039 <2439943130@qq.com>
2026-03-03 18:54:11 +0800

    Update single_bit_cdc.v

--- single_bit_cdc.v
+++ single_bit_cdc.v
@@ -7,7 +7,7 @@ output o_pluse_f
 )
 reg r_pluse_d0 ;
 reg r_pluse_d1 ;
-always@( posedge i_clk_s)begin//fast时钟域的频率超过slow时钟域的频率两倍以上；打两拍
+  always@( posedge i_clk_f)begin//fast时钟域的频率超过slow时钟域的频率两倍以上；打两拍
 r_pluse_do <= i_pluse_s ;
 r_pluse_d1 <= r_pluse_d0;
 end
